526f920be0b9872407ab1ec4628db7b34cc0577a
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-10-06 17:57:11 +0700

    add metagraph

--- metagraph/app.py
+++ metagraph/app.py
@@ -1,1 +1,20 @@
 import bittensor as bt
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+import os
+load_dotenv()
+app = Flask(__name__)
+
+@app.route('/metagraph', methods=['GET'])
+def get_status():
+    metagraph = bt.metagraph(int(os.getenv('netuid')), network=os.getenv('network'))
+    coldkeys = metagraph.coldkeys
+    axons = metagraph.axons
+    incentive_data = metagraph.incentive
+    array_incentive_data = incentive_data.numpy()
+    print(axons)
+    print(array_incentive_data)
+    return jsonify({ 'incentive_data': array_incentive_data.tolist(), 'coldkeys': coldkeys, 'axons': axons })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
