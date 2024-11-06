46020f96e71cacdb322c44c349dcb1273c07a3b7
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-11-06 15:21:35 +0700

    update endpoint

--- app.js
+++ app.js
@@ -4,22 +4,33 @@ const axios = require("axios");
 require("dotenv").config();
 const fetchMetagraph = async () => {
   try {
-    const listEndpoint = process.env.ENDPOINT_URL.split(",");
+    const listEndpoint = JSON.parse(process.env.ENDPOINT_URL);
     let listModel = [];
     for (let i = 0; i < listEndpoint.length; i++) {
       try {
-        const endpoint = listEndpoint[i];
-        const response = await axios.get(`http://${endpoint}/info`, {
-          timeout: 10000,
-        });
-        const model = response?.data?.model_name;
-        listModel.push(model || null);
+        const stack = listEndpoint[i];
+        let stackData = [];
+        const endpointData = await Promise.all(
+          stack.map(async (e) => {
+            try {
+              console.log(e);
+              const response = await axios.get(`http://${e}/info`, {
+                timeout: 1000,
+              });
+              const model = response?.data?.model_name;
+              stackData.push(model);
+            } catch (error) {
+              stackData.push(null);
+              console.log(error);
+            }
+          })
+        );
+        listModel.push(stackData);
       } catch (error) {
-        listModel.push(null);
         console.log(error);
       }
     }
-    axios({
+    await axios({
       baseURL: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`,
       url: "/sendMessage",
       method: "post",
@@ -27,11 +38,18 @@ const fetchMetagraph = async () => {
         chat_id: process.env.TELEGRAM_GROUP_ID,
         message_thread_id: process.env.TELEGRAM_THREAD_ID,
         text: `${listEndpoint
-          .map((e, index) => {
-            return `http://${e}/info ${listModel[index]} ${
-              listModel[index] == null ? process.env.TELEGRAM_TAG : ""
-            }\n`;
-          })
+          .map(
+            (e) =>
+              `
+--------------------------------
+${e
+  .map((j, index) => {
+    return `http://${j}/info ${listModel[index]} ${
+      listModel[index] == null ? process.env.TELEGRAM_TAG : ""
+    }\n`;
+  })
+  .join("")}`
+          )
           .join("")}`,
         parse_mode: "html",
         disable_web_page_preview: true,
@@ -101,3 +119,4 @@ cron.schedule("*/10 * * * *", () => {
   console.log("Fetching endpoint");
   fetchRegPrice();
 });
+
